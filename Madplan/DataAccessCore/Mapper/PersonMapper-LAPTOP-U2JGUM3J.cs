9e44e48d7835dd4a5f8614d86312d8bd133d7b50
S4uroNN <alin_constantin15@hotmail.com>
2023-05-25 13:45:24 +0200

    pc

---
+++ Madplan/DataAccessCore/Mapper/PersonMapper-LAPTOP-U2JGUM3J.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessCore.Mapper
+{
+    internal class PersonMapper
+    {
+        public static DTOCore.Model.Person Map(Person person)
+        {
+            return new DTOCore.Model.Person(person.Id, person.Name, person.Age);
+        }
+        public static Person Map(DTOCore.Model.Person person)
+        {
+            return new Person(person.Id, person.Name, person.Age);
+
+        }
+    }
+}
